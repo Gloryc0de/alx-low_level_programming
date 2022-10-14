289d04ac94c121f00666a7d7c19bbbed429d2875
Gloryc0de <glorysampson2024@gmail.com>
2022-10-14 20:47:38 +0100

    Fixed line issues from betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- *  main - with proper grammar, but the outcome is a piece of art,, followed by lin  *		e
+ *  main - with proper grammar, but the outcome is a piece of art,
+ *  followed by line
  *
  * Return: Always 0 (Success)
  *
