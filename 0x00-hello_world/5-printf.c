68213464d3837090be05980e1df91a31eb7e75b8
Gloryc0de <glorysampson2024@gmail.com>
2022-10-14 12:22:54 -0700

    Making new addition

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - "Programming is like building a multilingual puzzle
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+C programming language with betty style of coding
