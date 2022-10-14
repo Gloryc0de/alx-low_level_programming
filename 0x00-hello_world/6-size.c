affce95a7737d7be4dcca09345707fb716bf8564
Gloryc0de <glorysampson2024@gmail.com>
2022-10-14 22:13:58 +0100

    New changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 {
 	printf("Size of a char :%d bytes", sizeof(char));
 	printf("Size of an int :%d bytes",sizeof(int));
+	printf("Size of a long init :% bytes",sizeof(long int));
         printf("Size of a long int :%d bytes",sizeof(int));
 	printf("Size of a long long int :%d bytes",sizeof(long long int));
         printf("Size of a float :%d bytes",sizeof(float));
