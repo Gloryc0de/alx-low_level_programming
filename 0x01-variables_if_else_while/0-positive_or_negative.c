f6488dcfdd54609e997492e8c8bd0d6d3da314e5
Gloryc0de <glorysampson2024@gmail.com>
2022-10-14 17:25:00 -0700

    c programming

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ 
+ * main - program will assign a random number to the variable n. 
+
+ *
+
+ * Return: 0 
+
+ */
+
+int main(void)
+
+{
+	int n;
+printf(enter your Number)
+scanf("%d",& Num);
+}
+{
+	if(n>0)
+	printf("positive\n");
+}
+{
+	if(n=0)
+	printf("zero\n");
+}
+{
+	if(n>0)
+	printf("negativ\n");
+}
+{
+    return (0);
+
+}
