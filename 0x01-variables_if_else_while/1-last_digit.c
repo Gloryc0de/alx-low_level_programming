bd9a6ca7f8c45666b187ea3acec789bd07dffda2
Gloryc0de <glorysampson2024@gmail.com>
2022-10-16 15:29:32 -0700

    random number to a variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -  headers goes there
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+		printf("Last Digit of %d and is greater than 5\n", n);
+	else if (n == 0)
+		printf("Last Digit of %d and is 0\n", n);
+	else if (n < 6 & !0)
+		printf("Last Digit of %d and is less than 6 and not 0\n", n);
+
+
+	return (0);
+}
