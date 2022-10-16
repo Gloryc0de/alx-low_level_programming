e1f30d7e36470c80af09b6541bbc7d5b202a16ab
Gloryc0de <glorysampson2024@gmail.com>
2022-10-16 12:38:20 -0700

    makinsure the file are saved

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - prints exactly and that piece of art is useful" 
+ *     
+ * Return:1 (sucess)
+ *
+ *
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful - Dora Korpar,2015-10-19\n",) ;
+
+	return(1) ;
+}
