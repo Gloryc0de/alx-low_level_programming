5ac905b79a0a37c61d80ff3230bca3c6b25c1bae
Gloryc0de <glorysampson2024@gmail.com>
2022-10-16 15:39:20 -0700

    random number to a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last Digit of %d and is greater than 5\n", n);
 	else if (n == 0)
 		printf("Last Digit of %d and is 0\n", n);
-	else if (n < 6 & !0)
+	else if (n < 6 "&" "!=0")
 		printf("Last Digit of %d and is less than 6 and not 0\n", n);
 
 
