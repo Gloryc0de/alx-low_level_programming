857af46242e6e47ab69a6382c0980230f8b2c7d5
Gloryc0de <glorysampson2024@gmail.com>
2022-10-17 12:31:49 -0700

    programs that prints lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints alphabets in lower case
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar(*);
+
+	return (0);
+}
