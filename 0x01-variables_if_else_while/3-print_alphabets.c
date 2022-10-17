6c366350d6dd93f777d2b1e7ade80b78a2f3eac5
Gloryc0de <glorysampson2024@gmail.com>
2022-10-17 16:13:21 -0700

    corrections

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,13 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+
 	putchar(ch);
-	}
-	{
+
 	for (ch = "A"; ch <= "Z"; ch++)
+
 	putchar(ch);
-	}
+
 	putchar("\n");
 	return (0);
 }
