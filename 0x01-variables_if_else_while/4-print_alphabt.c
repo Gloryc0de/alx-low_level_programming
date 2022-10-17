a537ce94b7a781fbeb7c0e4aef71e4b51c0ead20
Gloryc0de <glorysampson2024@gmail.com>
2022-10-17 16:32:21 -0700

    lower case letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  prints alphabets in lower case except q $ e
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	if (ch != 'e' && ch != 'q')
+
+	putchar(ch);
+
+	putchar("\n");
+
+	return (0);
+}
