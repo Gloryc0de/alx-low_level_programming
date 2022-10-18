8ac525ab58063c3b86659bc59a8ca9272be2c684
Gloryc0de <glorysampson2024@gmail.com>
2022-10-17 17:15:47 -0700

    printing digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  prints alphabets in lower case except q $ e
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+
+	printf('%d', n);
+
+	printf('\n');
+
+	return (0);
+}
