7c200525a1ac6b0e13964e279b631295c14b47d8
Gloryc0de <glorysampson2024@gmail.com>
2022-10-17 19:34:07 -0700

    prints lower case in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program that prints alphabets in lower case
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
