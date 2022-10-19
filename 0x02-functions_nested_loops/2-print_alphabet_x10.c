fee9a2b9a0538de7ae5fbdf59ff866650bf0dbbb
Gloryc0de <glorysampson2024@gmail.com>
2022-10-19 21:15:31 +0000

    ten times of alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lower case followed by a new line
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		_putchar(ch);
+
+	}
+
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
+void print_alphabet_x10(void);
 void print_alphabet(void);
