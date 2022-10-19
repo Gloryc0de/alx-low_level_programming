cf31d39a8871d5319dfe8e97adc8d049588d356a
Gloryc0de <glorysampson2024@gmail.com>
2022-10-19 12:20:50 -0700

    Updating my snadbox info

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -17,7 +17,7 @@ void print_alphabet(void)
 
 	char ch;
 
-	for (ch = 'a'; ch = 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,31 +1,11 @@
-julien@ubuntu:~/0x02$ cat 1-main.c
-#include "main.h"
+
+# ifdef MAIN.H
+# define MAIN.H 
+
+ void print_alphabet(void);
 
 
-/**
 
- * main - check the code
 
- *
+# endif
 
- * Return: Always 0.
-
- */
-
-int main(void)
-
-{
-
-    print_alphabet();
-
-    return (0);
-
-}
-
-julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 1-main.c 1-alphabet.c -o 1-alphabet
-
-julien@ubuntu:~/0x02$ ./1-alphabet 
-
-abcdefghijklmnopqrstuvwxyz
-
-julien@ubuntu:~/0x02$
