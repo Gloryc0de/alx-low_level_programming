e18841e0d8b36627ede9cab37cac444d05f7bb76
Gloryc0de <glorysampson2024@gmail.com>
2022-10-20 17:08:04 +0000

    debugging 0-positive_or_negative .c task

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,13 +8,9 @@
  *
  */
 
-void positive_or_negative(int i);
+void positive_or_negative(int n)
 
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
