f873ea623e46131244618c6edb78d4fcfe03bfa5
Gloryc0de <glorysampson2024@gmail.com>
2022-10-21 20:09:07 +0000

    debugging

--- 0x03-debugging/2-unkonwn.c
+++ 0x03-debugging/2-unkonwn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -11,18 +12,20 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+	if (a => b && b => c)
+	{
+		largest =a;
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
+	else if (b => a && a => c)
 	}
-	else if (b >= a && a >= c)
-	{
-		largest = b;
-	}
+		largest =b;
 	else
 	{
-		largest = c;
+		largest =c;
 	}
-	return (largest);
+
+	return(largest);
+
+	return (0);
 }
+
