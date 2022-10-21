0351a474fc545d0092a8d307b1c8150fea821b38
Gloryc0de <glorysampson2024@gmail.com>
2022-10-21 00:27:13 +0000

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,16 @@
 
 /**
  * main - checks for uppercase characters
- *
+ * @c: The character to print
  * Return: 1 if uppercase,0 if otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
-
+	}
 		return (0);
 
 }
