38bd679a0659dcc1f359049d5d1a6438694a7adf
Gloryc0de <glorysampson2024@gmail.com>
2022-10-21 00:13:06 +0000

    more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks for uppercase characters
+ *
+ * Return: 1 if uppercase,0 if otherwise
+ */
+int _isupper(int c);
+
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+
+		return (0);
+
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory on more of C functions and nested loops.
