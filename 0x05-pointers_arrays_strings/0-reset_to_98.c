65a0386fce0cf050fc99d1d300596b92c8d3f626
Gloryc0de <glorysampson2024@gmail.com>
2022-10-24 21:36:54 +0000

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
 * main -function that takes a pointer to int and updates its value to  98
-*@n:input parameter
-* Return: Always 0.
+*@n: input parameter
+* Return: n.
 */
 
 void reset_to_98(int *n)
