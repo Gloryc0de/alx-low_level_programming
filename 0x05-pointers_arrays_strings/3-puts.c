af64e49d3248b7e1e6b1a089220098d71f7f7fa1
Gloryc0de <glorysampson2024@gmail.com>
2022-10-25 20:15:26 +0000

    strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <string.h>
 /**
-*_puts - A function that prints a string followed by a new line to stdout
+*_strlen - prints the length og a string followed by a new line to stdout
 *@str: string character
-*Return: 0
 */
 
-void _puts(char *str)
+ void _puts(char *str)
 {
 
-		_putchar(*str);
-		_putchar('\n');
+		while (*str != '\0')
+		{
+			str++;
 
+		_putchar(*str);
+		}
 
 }
-
