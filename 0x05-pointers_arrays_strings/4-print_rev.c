8f5c883295a6203d80029e406efebdf9c5ad2985
Gloryc0de <glorysampson2024@gmail.com>
2022-10-26 02:11:19 +0000

    pointers,strings and arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,26 @@
 
 void print_rev(char *s)
 {
-		int n = 0
-			;
-	while (*(s + n))
+
+	int n;
+
+	n = 0;
+	while (s[n] != '\0')
+
+	{
 		n++;
 
-		n = n - 1;
+	}
+
+	n = n - 1;
 
 	while (n >= 0)
 
-		{
-				_putchar(*(s + n));
-				n--;
-		}
+	{
+		_putchar(s[n]);
+		n--;
+
+	}
 
 		_putchar('\n');
 
