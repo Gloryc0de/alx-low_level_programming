1b6a0ba04f55df00bb107abaca3f70f92c77b655
Gloryc0de <glorysampson2024@gmail.com>
2022-10-28 01:13:04 +0000

    more pointers,strings and arrays

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,17 +7,17 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &n
+	p = &n;
 	/*
 	* add one line of code
 	* Do not use the variable a in your new line of cod
 	* not allowed to modify the variable p
 	* can only write one statement
+	* not allowed to use ,
 	* not allowed to code anytgthing else
 	*/
-
 	*(p + 5) = 98;
-	/* prints 98\n */
+	/* ... prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 
 	return (0);
