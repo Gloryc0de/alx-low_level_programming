970b4f432634e1c8a923486b64e92ee2ce83a25e
Gloryc0de <glorysampson2024@gmail.com>
2022-10-28 00:50:53 +0000

    more pointers,strings and arrays

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main -program that prints integers
+* Do not use the variable a in your new line of code
+* not allowed to modify the variable p
+* can only write one statement
+* not allowed to use ,
+* return: always 0
+*/
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 98;
+	p = &n;
+	*(p + 5) = 98;
+	/* prints 98\n */
+	printf("a[2] = %d\n" a[2];
+
+	return (0);
+}
