9958664930cb2e5f8608368cedf90703c271bbcf
Gloryc0de <glorysampson2024@gmail.com>
2022-11-09 18:57:05 +0000

    arguements to main

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - A program that prints all arguements as it receives.
+*@argc : arguement count
+*@argv : array of strings
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+ 	int n;
+
+	for (n=0;n<argc;n++)
+
+		printf("%s\n", argv[n]);
+
+	return (0);
+}
