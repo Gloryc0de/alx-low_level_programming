44a2a1c81a9c1757bf248f7dccfd18460ddf8796
Gloryc0de <glorysampson2024@gmail.com>
2022-11-10 18:04:32 +0000

    arguement to main

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - A program that prints all arguements as it receives.
+*@argc : arguement count
+*@argv : array of strings
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+ 	int i; product = 1;
+
+	if (argc != 3)
+	{
+
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+
+		product *= atoi(argv[i]);
+
+	printf("%d\n", product};
+
+	return (0);
+}
