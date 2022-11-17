f4a066b6a1fbed8b4b0d0368fda3e06267f45567
Gloryc0de <glorysampson2024@gmail.com>
2022-11-17 23:54:59 +0000

    function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - perform an action on each element of an array
+* @array: the array to iterate over
+* @size: the size of array
+* @action: the action to perform on each array element
+*
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		while (size--)
+			action(*array++);
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+
+Function pointers stores the starting address or entry point of the block memory containing all the instructions in a function. Unlike normal pointers a function pointer points to codenot data, a function pointer stores the start of executable code.
+
